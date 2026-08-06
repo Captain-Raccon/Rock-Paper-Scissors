d854388faa9d9f78cb778c02a64e0bc96655efe4
Renas <178157966+Captain-Raccon@users.noreply.github.com>
2026-08-06 17:58:52 +0300

    Create functions what get Computer and user choice and convert it to words

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
 //Псевдокод;
+
+const arr = ["Rock", "Paper", "Scissors"];
+
+let userScore = 0;
+let compScore = 0;
+
+function getComputerChoice() {
+  let random = Math.floor(Math.random() * 3) + 1;
+  let result = arr[random - 1];
+  return result;
+}
+
+function getUserChoice(value) {
+  let result = null;
+  if (value) {
+    result = arr[value];
+    return result;
+  } else {
+    return "error";
+  }
+}
+
+console.log(getUserChoice(1));
+console.log(getComputerChoice());
+
 // Массив с значениями
 //// две переменные которые хранят счет, победы компьютера и человека
 
